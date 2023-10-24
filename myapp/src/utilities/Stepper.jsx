2da92588847238a908f3e3a96b0b831a638dd2fa
Erling Holte <erlinhol@uio.no>
2023-10-24 18:51:11 +0200

    :lipstick: Add Stepper
    A start for how the stepper component would be

--- myapp/src/utilities/Stepper.jsx
+++ myapp/src/utilities/Stepper.jsx
@@ -1,21 +1,51 @@
 import React from "react";
-import {Modal, ModalTitle, ModalContent, ModalActions, Button, ButtonStrip} from '@dhis2/ui'
+import {Modal, ModalTitle, ModalContent, ModalActions, Button, ButtonStrip, Chip} from '@dhis2/ui'
+import Search from "./Search";
+
+const Step = (props) => {
+    return (
+        <div style={{display: 'flex', gap: '10px', alignItems: 'center'}}>
+            <Chip>
+                {props.step}
+            </Chip>
+            <div style={{ display: 'flex', flexDirection: 'column'}}>
+                <p>Step {props.step}</p>
+                <p>{props.step_name}</p>
+            </div>
+        </div>
+    )
+}
+
 const Stepper = (props) => {
     
+    /* props.step1, props.step2, props.step3 */
     return (<Modal large>
         <ModalTitle>
-            This is a large modal with title, content and primary action
+            {props.title}
         </ModalTitle>
         <ModalContent>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            <div style={{display: 'flex', gap: '10px'}}>
+                {/* Steps: */}
+                <div style={{display: 'flex', gap: '20px', flexDirection: 'column'}}>
+                    <Step step={1} step_name={'Commodity'}></Step>
+                    <Step step={2} step_name={'Additional information'}></Step>
+                    <Step step={3} step_name={'Dispensing summary'}></Step>
+                </div>
+                {/* Main section */}
+                <div>
+                    <h3>Commodity section</h3>
+                    <Search placeholder='Search commodity' width={'320px'}></Search>
+                </div>
+            </div>
+            
         </ModalContent>
         <ModalActions>
             <ButtonStrip end>
                 <Button onClick={() => console.log("Hei")} secondary>
-                    Secondary action
+                    Previous
                 </Button>
                 <Button onClick={() => console.log("Hei")} primary>
-                    Primary action
+                    Next
                 </Button>
             </ButtonStrip>
         </ModalActions>
