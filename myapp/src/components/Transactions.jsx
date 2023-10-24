284429570ee57d8273f3f346baf82367d60b0977
Erling Holte <erlinhol@uio.no>
2023-10-24 15:57:55 +0200

    :art: Implement data interface for Transactions

--- myapp/src/components/Transactions.jsx
+++ myapp/src/components/Transactions.jsx
@@ -2,8 +2,18 @@ import { Button } from '@dhis2/ui'
 import React from "react";
 import Search from '../utilities/Search';
 import Dropdown from '../utilities/Dropdown';
+import TransactionsForDay from './TransactionsForDay'
 const Transactions = () => {
+
+    /* TODO: replace with actual transaction data */
+    const transaction_by_day = {date: 'October 13, 2023', 
+                                transactions: 
+                                [{commodity_name: 'Zink', time: '8:30pm', sender: 'George Slater', reciever: 'Ralph Hans', amount: '-163', new_stock: '243' },
+                                {commodity_name: 'Ibuprofen', time: '9:30pm', sender: 'George Slater', reciever: 'Ralph Hans', amount: '-50', new_stock: '200' }
+                            ]}
+
     return(<>
+            {/* Navigation buttons to add stock or new dispensing */}
             <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <h1 style={{ display: 'inline', margin: 0 }}>Transactions</h1>
                 <div style={{ display: 'flex', gap: '10px'}}>
@@ -17,12 +27,17 @@ const Transactions = () => {
                 </div>
             </div>
             <p></p>
+            {/* The different search and filter options */}
             <div style={{display: 'flex', gap: '10px'}}>
             <Search placeholder='Search commodity' width={'320px'}></Search>
             <Dropdown placeholder='Period'></Dropdown>
             <Dropdown placeholder='All transactions'></Dropdown>
             <Dropdown placeholder='Recipient'></Dropdown>
             </div>
+
+            {/* Multiple transactions can be listed here: */}
+            <TransactionsForDay date={transaction_by_day.date} transactions={transaction_by_day.transactions}></TransactionsForDay>
+            <TransactionsForDay date={transaction_by_day.date} transactions={transaction_by_day.transactions}></TransactionsForDay>
             
         </>)
  }
---
+++ myapp/src/components/TransactionsForDay.jsx
@@ -0,0 +1,27 @@
+import { Menu, IconArrowRight24, IconMore24  } from '@dhis2/ui'
+import React from "react";
+const TransactionsForDay = (props) => {
+    /* Displays the transactions for a chosen day.
+    The data is passed in through props */
+    return(<>
+            <h2>{props.date}</h2>
+            {props.transactions.map((transaction, i) =>
+                <Menu key={i}>
+                    <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <p>{transaction.commodity_name}</p>
+                        <p>{transaction.time}</p>
+                        <p>{transaction.sender}</p>
+                        <IconArrowRight24></IconArrowRight24>
+                        <p>{transaction.recipient}</p>
+                        <div>
+                            <p>{transaction.amount}</p>
+                            <p>Updated stock: {transaction.new_stock}</p>
+                        </div>
+                        <IconMore24></IconMore24>
+                    </div>     
+                </Menu>
+            )}
+        </>)
+ }
+
+export default TransactionsForDay
