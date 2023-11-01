604c1f668aa4eb543a1447ab2763f7f167ce0976
Erling Holte <erlinhol@uio.no>
2023-11-01 18:27:13 +0100

    Implement search for parts of commodity name (splitted by whitespace)

--- myapp/src/search.js
+++ myapp/src/search.js
@@ -1,7 +1,11 @@
 function search(element, searchWord) {
-  return element.commodityName
-    .toLowerCase()
-    .startsWith(searchWord.toLowerCase());
+  const words = element.commodityName.split(/(\s+)/);
+  for (const word of words) {
+    if (word.toLowerCase().startsWith(searchWord.toLowerCase())) {
+      return true; // Return true if any word matches the search term
+    }
+  }
+  return false; // Return false if no word matches the search term
 }
 
 export function filterBySearch(list, searchWord) {
