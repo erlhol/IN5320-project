117085e75869c90c6accaf0c9bff0defbf998441
Erling Holte <erlinhol@uio.no>
2023-11-10 12:58:53 +0100

    Add ApexChart to npm

--- myapp/src/components/stockOverview/StockDetail.jsx
+++ myapp/src/components/stockOverview/StockDetail.jsx
@@ -8,6 +8,7 @@ import {
   IconCalendar24,
 } from "@dhis2/ui";
 import classes from "../../App.module.css";
+import ConsumptionHistoryChart from "./ConsumptionHistoryChart";
 const StockInfo = props => {
   return (
     <div className={classes.stockInfo}>
@@ -23,28 +24,30 @@ const StockInfo = props => {
 const StockDetail = props => {
   return (
     <Modal onClose={() => props.onClose(null)}>
-      <h1>{props.selectedStock.commodityName}</h1>
-      <div className={classes.modalContainer}>
-        <StockInfo
-          infoString={"Stock balance"}
-          infoValue={props.selectedStock.endBalance}
-          icon={<IconDimensionData16></IconDimensionData16>}
-        ></StockInfo>
+      <ModalContent>
+        <h2>{props.selectedStock.commodityName}</h2>
+        <div className={classes.modalContainer}>
+          <StockInfo
+            infoString={"Stock balance"}
+            infoValue={props.selectedStock.endBalance}
+            icon={<IconDimensionData16></IconDimensionData16>}
+          ></StockInfo>
 
-        <StockInfo
-          infoString={"Consumption"}
-          infoValue={props.selectedStock.consumption}
-          icon={<IconArrowDown24></IconArrowDown24>}
-        ></StockInfo>
+          <StockInfo
+            infoString={"Consumption"}
+            infoValue={props.selectedStock.consumption}
+            icon={<IconArrowDown24></IconArrowDown24>}
+          ></StockInfo>
 
-        <StockInfo
-          infoString={"Last dispensing"}
-          infoValue={props.selectedStock.lastDispensing + "10/13/2023"}
-          icon={<IconCalendar24></IconCalendar24>}
-        ></StockInfo>
-      </div>
-
-      <ModalContent>StockDetail</ModalContent>
+          <StockInfo
+            infoString={"Last dispensing"}
+            infoValue={props.selectedStock.lastDispensing + "10/13/2023"}
+            icon={<IconCalendar24></IconCalendar24>}
+          ></StockInfo>
+        </div>
+        <h3>Consumption History</h3>
+        <ConsumptionHistoryChart></ConsumptionHistoryChart>
+      </ModalContent>
     </Modal>
   );
 };
