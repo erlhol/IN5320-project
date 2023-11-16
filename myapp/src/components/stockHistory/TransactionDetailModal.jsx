2b7f75380fa2bec4655925ca89526ff08de84bde
Finja-Malin Scheltens <scheltens@id-on.de>
2023-11-17 00:36:57 +0100

    add changes from other detail view branch

--- myapp/src/components/stockHistory/TransactionDetailModal.jsx
+++ myapp/src/components/stockHistory/TransactionDetailModal.jsx
@@ -22,48 +22,50 @@ export default function TransactionDetailModal(props) {
   return (
     <Modal onClose={() => props.onClose()} position="middle" large>
       <ModalTitle>Transaction Detail</ModalTitle>
-      <ModalContent>
+      <ModalContent className={classes.transDetailModalContent}>
         <div className={classes.detailViewInfoBoxesContainer}>
           <DetailViewInfoBox
             infoString={"Date and Time"}
             infoValue={props.transaction.date + " " + props.transaction.time}
             icon={<IconCalendar24 />}
-          ></DetailViewInfoBox>
+          />
           <DetailViewInfoBox
             infoString={"Recipient"}
             infoValue={props.transaction.dispensedTo}
             icon={<IconLogOut24 />}
-          ></DetailViewInfoBox>
+          />
         </div>
-        <div className={classes.transDetailTableCaption}>
-          {props.transType} Commodities
+        <div>
+          <div className={classes.transDetailTableCaption}>
+            {props.transType} Commodities
+          </div>
+          <Table className={classes.transDetailTable}>
+            <TableHead>
+              <TableRowHead>
+                <TableCellHead width={spacers.dp250} className={classes.bold}>
+                  Commodity
+                </TableCellHead>
+                <TableCellHead className={classes.bold}>
+                  {props.transType} amount
+                </TableCellHead>
+                <TableCellHead className={classes.bold}>
+                  Updated stock
+                </TableCellHead>
+              </TableRowHead>
+            </TableHead>
+            <TableBody>
+              {props.transaction.commodities.map((commodity, i) => (
+                <TableRow key={i}>
+                  <TableCell width={spacers.dp250}>
+                    {commodity.commodityName}
+                  </TableCell>
+                  <TableCell>{commodity.amount}</TableCell>
+                  <TableCell>{commodity.balanceAfterTrans}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </div>
-        <Table>
-          <TableHead>
-            <TableRowHead>
-              <TableCellHead width={spacers.dp250} className={classes.bold}>
-                Commodity
-              </TableCellHead>
-              <TableCellHead className={classes.bold}>
-                {props.transType} Amount
-              </TableCellHead>
-              <TableCellHead className={classes.bold}>
-                Updated Stock
-              </TableCellHead>
-            </TableRowHead>
-          </TableHead>
-          <TableBody>
-            {props.transaction.commodities.map((commodity, i) => (
-              <TableRow key={i}>
-                <TableCell width={spacers.dp250}>
-                  {commodity.commodityName}
-                </TableCell>
-                <TableCell>{commodity.amount}</TableCell>
-                <TableCell>{commodity.balanceAfterTrans}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
       </ModalContent>
     </Modal>
   );
